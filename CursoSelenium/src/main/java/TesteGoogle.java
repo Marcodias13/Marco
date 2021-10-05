4a69d63add68e43b9eda2708e1844ac605bf6201
Marco <marcodias13@hotmail.com>
2021-10-05 11:04:55 -0300

    Exercícios com campo de treinamento

--- CursoSelenium/src/main/java/TesteGoogle.java
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -1,12 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+	@Test
+	public void teste() {
+		WebDriver driver = new FirefoxDriver();
 		driver.get("http://google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 
 }
