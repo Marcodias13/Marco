783d7d67c1f5637834ef04943d5178b42a64406e
Marco <marcodias13@hotmail.com>
2021-10-18 09:52:15 -0300

    Cenário login, cadastro e consulta + screenshot

--- CursoCucumber/src/test/java/telaCadastroShopping.java
+++ CursoCucumber/src/test/java/telaCadastroShopping.java
@@ -1,8 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
@@ -19,7 +30,7 @@ public class telaCadastroShopping {
 
 	@Quando("^clico no ícone de user$")
 	public void clicoNoÍconeDeUser() throws Throwable {
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 	    driver.findElement(By.id("menuUser")).click();
 	}
 
@@ -42,6 +53,7 @@ public class telaCadastroShopping {
 
 	@Quando("^clico em sign in$")
 	public void clicoEmSignIn() throws Throwable {
+		Thread.sleep(5000);
 	    driver.findElement(By.tagName("button")).click();
 	    
 	}
@@ -103,5 +115,102 @@ public class telaCadastroShopping {
 		Thread.sleep(3000);
 		driver.findElement(By.name("phone_numberRegisterPage")).sendKeys(arg1);
 	}
+	
+	@Quando("^preencho country \"([^\"]*)\"$")
+	public void preenchoCountry(String arg1) throws Throwable {
+		Thread.sleep(3000);
+		WebElement element = driver.findElement(By.name("countryListboxRegisterPage"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText(arg1);
+	}
+
+	@Quando("^preencho city \"([^\"]*)\"$")
+	public void preenchoCity(String arg1) throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("cityRegisterPage")).sendKeys(arg1);    
+	}
+
+	@Quando("^preencho adress \"([^\"]*)\"$")
+	public void preenchoAdress(String arg1) throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("addressRegisterPage")).sendKeys(arg1); 
+	}
+
+	@Quando("^preencho state \"([^\"]*)\"$")
+	public void preenchoState(String arg1) throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("state_/_province_/_regionRegisterPage")).sendKeys(arg1); 
+	}
+
+	@Quando("^preencho postal code \"([^\"]*)\"$")
+	public void preenchoPostalCode(String arg1) throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("postal_codeRegisterPage")).sendKeys(arg1);
+	}
+	
+	@Quando("^aceito os termos de condições de uso e privacidade$")
+	public void aceitoOsTermosDeCondiçõesDeUsoEPrivacidade() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("i_agree")).click();
+	}
+
+	@Então("^botão para registrar conta é exibido com sucesso$")
+	public void botãoParaRegistrarContaÉExibidoComSucesso() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.id("register_btnundefined"));
+	    
+	    Assert.assertEquals("REGISTER", driver.findElement(By.id("register_btnundefined")).getText());
+	}
+	
+	@Quando("^clico no botão para registrar conta$")
+	public void clicoNoBotãoParaRegistrarConta() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.id("register_btnundefined")).click();
+	}
+
+	@Então("^conta logada com sucesso$")
+	public void contaRegistradaComSucesso() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.id("menuUserLink"));
+	    
+	    Assert.assertEquals("testemarco1", driver.findElement(By.id("menuUserLink")).getText());
+	}
+	
+	@Quando("^clico em algum produto$")
+	public void clicoEmAlgumProduto() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.id("tabletsTxt")).click();
+	}
+
+	@Então("^opção de compra é exibida com sucesso$")
+	public void opçãoDeCompraÉExibidaComSucesso() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.name("buy_now"));
+	    
+	    Assert.assertEquals("BUY NOW", driver.findElement(By.name("buy_now")).getText());
+	}
+	
+	
+	@Before
+	public void inicializa() {
+		System.out.println("Começando aqui");
+	}
+	
+	@After(order = 1)
+	public void screenshot(Scenario cenario) {
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(file, new File("target/screenshot/"+cenario.getId()+".jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@After(order = 0)
+	public void fecharBrowser() {
+		driver.quit();
+	}
+	
 
 }
--- CursoCucumber/src/test/resources/tela_cadastro_shopping.feature
+++ CursoCucumber/src/test/resources/tela_cadastro_shopping.feature
@@ -6,14 +6,13 @@ Gostaria de me cadastrar no site de compras
 Para que eu possa ter acesso ao itens disponíveis
 
 @ignore
-Cenário: Deve acessar tela de login
+Cenário: Deve acessar tela de login --OK
 Dado acesso o link de compras online
 Quando clico no ícone de user
 Então tela de login é exibida com sucesso
 
-
 @ignore
-Cenário: Deve preencher tela de login sem usuário cadastrado
+Cenário: Deve preencher tela de login sem usuário cadastrado --OK
 Dado acesso o link de compras online
 Quando clico no ícone de user
 E informo o usuário "a@a"
@@ -21,8 +20,8 @@ E a senha "a"
 E clico em sign in
 Então sou notificado de que o nome e senha estão incorretos
 
-
-Cenário: Deve criar nova conta
+@ignore
+Cenário: Deve preencher os dados na tela de cadastro  --OK
 Dado acesso o link de compras online
 Quando clico no ícone de user
 E clico em criar nova conta
@@ -33,29 +32,50 @@ E preencho confirm password "Marco14"
 E preencho first name "Marco"
 E preencho last name "Dias"
 E preencho phone number "5555-5555"
-Então tela de login é exibida com sucesso
+E preencho country "Brazil"
+E preencho city "São Paulo"
+E preencho adress "Rua teste, 555"
+E preencho state "SP"
+E preencho postal code "00000-000"
+E aceito os termos de condições de uso e privacidade
+Então botão para registrar conta é exibido com sucesso
 
 @ignore
-Cenário: Não deve inserir uma conta sem nome
-Dado que estou acessando a aplicação
-Quando informo o usuário "a@a"
-E a senha "a"
-E seleciono entrar
-Então visualizo a página inicial
-Quando seleciono Contas
-E seleciono Adicionar
-E seleciono Salvar
-Então sou notificar que o nome da conta é obrigatório
+Cenário: Deve preencher os dados na tela de cadastro e criar nova conta --OK
+Dado acesso o link de compras online
+Quando clico no ícone de user
+E clico em criar nova conta
+E preencho Username "testemarco1"
+E preencho email "testemarco1@teste.com"
+E preencho password "Testemarco1"
+E preencho confirm password "Testemarco1"
+E preencho first name "TesteM"
+E preencho last name "Teste"
+E preencho phone number "5555-5555"
+E preencho country "Brazil"
+E preencho city "São Paulo"
+E preencho adress "Rua teste, 555"
+E preencho state "SP"
+E preencho postal code "00000-000"
+E aceito os termos de condições de uso e privacidade
+E clico no botão para registrar conta
+Então conta logada com sucesso
 
 @ignore
-Cenário: Não deve inserir uma conta com nome já existente
-Dado que estou acessando a aplicação
-Quando informo o usuário "a@a"
-E a senha "a"
-E seleciono entrar
-Então visualizo a página inicial
-Quando seleciono Contas
-E seleciono Adicionar
-E informo a conta "Conta de Teste"
-E seleciono Salvar
-Então sou notificado que já existe uma conta com esse nome
+Cenário: Deve logar com sucesso --OK
+Dado acesso o link de compras online
+Quando clico no ícone de user
+E informo o usuário "testemarco1"
+E a senha "Testemarco1"
+E clico em sign in
+Então conta logada com sucesso
+
+@ignore
+Cenário: Deve logar e consultar produto --OK
+Dado acesso o link de compras online
+Quando clico no ícone de user
+E informo o usuário "testemarco1"
+E a senha "Testemarco1"
+E clico em sign in
+E clico em algum produto
+Então opção de compra é exibida com sucesso
